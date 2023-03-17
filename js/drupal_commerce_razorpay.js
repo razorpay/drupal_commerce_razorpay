0b29340f4b8f2bfde9ec470cb04d0fc20b14c968
abdulwahidsharief <abdul.sharief@razorpay.com>
2023-03-17 10:11:59 +0530

    pay now and cancel buttons

--- js/drupal_commerce_razorpay.js
+++ js/drupal_commerce_razorpay.js
@@ -1,6 +1,45 @@
-(function($) {
-  Drupal.behaviors.razorpay_drupal = {
-    attach: function(context, settings) {
+(function ($, Drupal, drupalSettings) {
+  'use strict';
+  Drupal.behaviors.drupal_commerce_razorpay = {
+    attach: function (context) {
+
+      var data = drupalSettings.drupal_commerce_razorpay;
+
+      var setDisabled = function(id, state) {
+        if (typeof state === 'undefined') {
+          state = true;
+        }
+        var elem = document.getElementById(id);
+        if (state === false) {
+          elem.removeAttribute('disabled');
+        } else {
+          elem.setAttribute('disabled', state);
+        }
+      };
+
+      data.modal = {
+        ondismiss: function() {
+          setDisabled('btn-razorpay', false);
+        },
+      };
+
+      function rzpOpenCheckout() {
+        setDisabled('btn-razorpay');
+        razorpayCheckout.open();
+      }
+
+      var razorpayCheckout = new Razorpay(data);
+      rzpOpenCheckout();
+
+      $('#btn-razorpay').on('click', function(event) {
+        event.preventDefault();
+        rzpOpenCheckout();
+      });
+
+      $('#btn-razorpay-cancel').on('click', function(event) {
+        event.preventDefault();
+      });
+
     }
   };
-}(jQuery));
+}(jQuery, Drupal, drupalSettings));
