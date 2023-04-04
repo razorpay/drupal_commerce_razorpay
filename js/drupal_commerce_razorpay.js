1cd51a42a20a80e429464220021bb07839f4e85c
abdulwahidsharief <abdul.sharief@razorpay.com>
2023-04-04 15:09:15 +0530

    processing wait alert

--- js/drupal_commerce_razorpay.js
+++ js/drupal_commerce_razorpay.js
@@ -1,9 +1,11 @@
 (function ($, Drupal) {
   'use strict';
-
     Drupal.behaviors.commerceRazorpayCheckout = {
         attach: function (context, drupalSettings) {
             once('commerceRazorpayCheckout', 'html').forEach(function (element) {
+
+                $("#msg-razorpay-success").css("background-color", "yellow");
+                $('#msg-razorpay-success').hide();
 
                 var data = drupalSettings.razorpay_checkout_data;
 
@@ -23,6 +25,15 @@
                     ondismiss: function() {
                         setDisabled('btn-razorpay', false);
                     },
+                };
+
+                data.handler = function(payment) {
+                    setDisabled('btn-razorpay-cancel');
+                    $('#msg-razorpay-success').show();
+                    $('#razorpay_order_id').val(payment.razorpay_order_id);
+                    $('#razorpay_payment_id').val(payment.razorpay_payment_id);
+                    $('#razorpay_signature').val(payment.razorpay_signature);
+                    $('#rzp_submit_button').click();
                 };
 
                 var razorpayCheckout = new Razorpay(data);
